eb78ba145267746eb0d839a9cefaf6b745e2ae7e
Manoj Panta <manojpanta95@gmail.com>
2018-02-22 21:17:26 -0700

    mastermind file is refactoredgit add .

--- test/mastermind_test.rb
+++ test/mastermind_test.rb
@@ -10,8 +10,6 @@ class MastermindTest < MiniTest::Test
   def test_if_exists
     combination = Combination.new
     guess = Guess.new("mano", combination)
-
-
     mastermind = Mastermind.new
     assert_instance_of Mastermind, mastermind
   end
@@ -20,24 +18,6 @@ class MastermindTest < MiniTest::Test
     combination = Combination.new
     guess = Guess.new("rgby", combination)
     mastermind = Mastermind.new
-    require'pry' ; binding.pry
     assert_equal 1, mastermind.record_guesses(guess)
-    assert_equal "Incorrect.", mastermind.initial_input
-
-
   end
-
-
-
-  # def test_right_guesses
-  #   assert_equal "p", @mastermind.initial_response("p")
-  # end
-
-
-
-
-
-
-
-
 end
