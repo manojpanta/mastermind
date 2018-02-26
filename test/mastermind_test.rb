ab0c8c63b94c2d9f4e1b6983febc3adf0d50d5d4
Manoj Panta <manojpanta95@gmail.com>
2018-02-25 23:44:13 -0700

    testing

--- test/mastermind_test.rb
+++ test/mastermind_test.rb
@@ -1,23 +1,23 @@
-require 'minitest'
-require 'minitest/autorun'
-require 'minitest/pride'
-require './lib/mastermind'
-require './lib/combination'
-
-class MastermindTest < MiniTest::Test
-
-  def test_if_exists
-    combination = Combination.new
-    guess = Guess.new("mano", combination)
-    mastermind = Mastermind.new
-    assert_instance_of Mastermind, mastermind
-  end
-
-  def test_guesses
-    combination = Combination.new
-    guess = Guess.new("rgby", combination)
-    mastermind = Mastermind.new
-    assert_equal 1, mastermind.record_guesses(guess)
-  end
-end
-i am on manoj brnach
+# require 'minitest'
+# require 'minitest/autorun'
+# require 'minitest/pride'
+# require './lib/mastermind'
+# require './lib/combination'
+#
+# class MastermindTest < MiniTest::Test
+#
+#   def test_if_exists
+#     combination = Combination.new
+#     guess = Guess.new("mano", combination)
+#     mastermind = Mastermind.new
+#     assert_instance_of Mastermind, mastermind
+#   end
+#
+#   def test_guesses
+#     combination = Combination.new
+#     guess = Guess.new("rgby", combination)
+#     mastermind = Mastermind.new
+#     assert_equal 1, mastermind.record_guesses(guess)
+#   end
+# end
+# i am on manoj brnach
